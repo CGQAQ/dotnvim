c1fb78e77014a2935052985e2ebe5a94db46059d
CGQAQ <m.jason.liu@outlook.com>
2024-02-19 15:56:32 +0800

    f2-f8 map to 1-7 in harpoon list

--- lua/cgqaq/configs/harpoon.lua
+++ lua/cgqaq/configs/harpoon.lua
@@ -5,10 +5,13 @@ print(vim.g.mapleader)
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<F5>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<F6>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<F7>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<F8>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<F2>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<F3>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<F4>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<F5>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<F6>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<F7>", function() harpoon:list():select(6) end)
+vim.keymap.set("n", "<F8>", function() harpoon:list():select(7) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-k>", function() harpoon:list():prev() end)
